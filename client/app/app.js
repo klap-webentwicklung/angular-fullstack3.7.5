6d569abdbe32fc7d43fe5e7957a7684eb60a7923
Marinus <marinus@klap-webdevelopment.com>
2018-06-05 16:59:27 +0200

    iplemented one language

--- client/app/app.js
+++ client/app/app.js
@@ -2,10 +2,25 @@
 
 angular.module('weindbApp', ['weindbApp.auth', 'weindbApp.admin', 'weindbApp.constants',
     'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
-    'validation.match'
+    'validation.match', 'pascalprecht.translate'
   ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, $translateProvider) {
     $urlRouterProvider.otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    // add translation table
+  $translateProvider
+  .translations('en', translations)
+  .preferredLanguage('en');
+
+
   });
+
+  var translations = {
+    HEADLINE: 'What an awesome module!',
+    PARAGRAPH: 'Srsly!',
+    NAMESPACE: {
+      PARAGRAPH: 'And it comes with awesome features!'
+    }
+  };
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,10 +4,12 @@
 
   class MainController {
 
-    constructor($http, $scope, socket) {
+    constructor($http, $scope, socket, $translate, $rootScope) {
       this.$http = $http;
       this.socket = socket;
       this.awesomeThings = [];
+      this.$translate = $translate;
+      this.$rootScope = $rootScope;
 
       $scope.$on('$destroy', function() {
         socket.unsyncUpdates('thing');
@@ -15,6 +17,36 @@
     }
 
     $onInit() {
+
+      var self = this;
+
+      this.$rootScope.$on('$translateChangeSuccess', function () {
+        this.$translate('HEADLINE').then(function (translation) {
+          self.headlineOnRootscope = translation;
+        }, function (translationId) {
+          self.headlineOnRootscope = translationId;
+        });
+      });
+
+      this.$translate('HEADLINE').then(function (headline) {
+        self.headline = headline;
+        console.log('​MainController -> $onInit -> self.headline:', self.headline);
+        
+      }, function (translationId) {
+        this.headline = translationId;
+        console.log('​MainController -> $onInit -> this.headline', this.headline);
+      });
+      this.$translate('PARAGRAPH').then(function (paragraph) {
+        self.paragraph = paragraph;
+      }, function (translationId) {
+        this.paragraph = translationId;
+      });
+      this.$translate('NAMESPACE.PARAGRAPH').then(function (anotherOne) {
+        self.namespaced_paragraph = anotherOne;
+      }, function (translationId) {
+        this.namespaced_paragraph = translationId;
+      });
+
       this.$http.get('/api/things')
         .then(response => {
           this.awesomeThings = response.data;
